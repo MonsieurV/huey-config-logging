fbf95f62a695a1d659517437554066ce24cef550
Yoan Tournade <yoan@ytotech.com>
2019-08-25 14:35:06 +0200

    Show effect of duplicate logging

--- demo.py
+++ demo.py
@@ -5,7 +5,13 @@ huey = SqliteHuey(filename='/tmp/demo.db')
 
 LOGGER = logging.getLogger("huey")
 
-# TODO How to disable altogether the huey default formatter and handler.
+# TODO How to disable altogether the huey default formatter and handler?
+
+# Add our own handler and formatter.
+custom_handler = logging.StreamHandler()
+custom_logformat = '[%(levelname)s:%(name)s:%(threadName)s] %(message)s'
+custom_handler.setFormatter(logging.Formatter(custom_logformat))
+LOGGER.addHandler(custom_handler)
 
 @huey.task()
 def add(a, b):
